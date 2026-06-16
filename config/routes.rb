724dc75fc901cf05bbffb6217885f1ed47aa7036
Katie Wood <kwood6319@hotmail.co.uk>
2026-06-16 18:53:09 +0900

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
-  get "turns/create"
-  get "games/new"
-  get "games/create"
-  get "games/show"
-  get "games/destroy"
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "games#new"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :games, only: [:new, :create, :show, :destroy] do
+    resources :turns, only: [:create]
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
