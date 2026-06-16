581f621a2055173d680804c14ed73b50d7586a8f
Katie Wood <kwood6319@hotmail.co.uk>
2026-06-16 20:51:56 +0900

    Added random colours to game#new team selection

--- app/javascript/controllers/teams_controller.js
+++ app/javascript/controllers/teams_controller.js
@@ -1,13 +1,32 @@
-// app/javascript/controllers/teams_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["container", "template"]
+  static targets = ["container"]
 
   addTeam() {
-    const fields = document.querySelector(".team-fields").cloneNode(true)
-    // Clear the cloned values
-    fields.querySelectorAll("input[type=text]").forEach(i => i.value = "")
-    document.getElementById("teams-container").appendChild(fields)
+    const teamCount = this.containerTarget.querySelectorAll(".team-fields").length + 1
+    const clone = this.containerTarget.querySelector(".team-fields").cloneNode(true)
+
+    // Set placeholder to "Team N"
+    clone.querySelectorAll("input[type=text], input:not([type])").forEach(i => {
+      i.value = ""
+      i.placeholder = `Team ${teamCount}`
+    })
+
+    // Assign a random colour
+    clone.querySelectorAll("input[type=color]").forEach(i => {
+      i.value = this.randomColour()
+    })
+
+    this.containerTarget.appendChild(clone)
+  }
+
+  randomColour() {
+    const colours = [
+      "#E63946", "#2A9D8F", "#E9C46A", "#F4A261",
+      "#457B9D", "#8338EC", "#FB5607", "#3A86FF",
+      "#06D6A0", "#FF006E", "#FFBE0B", "#8ECAE6"
+    ]
+    return colours[Math.floor(Math.random() * colours.length)]
   }
 }
